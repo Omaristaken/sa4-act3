42927fadd9b2e50185cd01ad3f96ff28351a0993
Omar <bomarm567@gmail.com>
2024-02-08 11:09:18 -0600

    Limits the number of guesses user can try

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,7 @@
 number = 10
 
 again = True
-count = 5
+trys = 3
 
 while again != 'q':
     print("I'm thinking of a number...")
@@ -11,15 +11,19 @@ while again != 'q':
         print("Congratulations! You guessed the right number.")
         break
  
-    if count > 0:
-        if guess > number:
-            print('Your guess was higher than the number.')
-        else:
-            print('Your guess was lower than the number.')
-        count -= 1
-        print(f'You have {count} hints left')
+    
+    if guess > number:
+        print('Your guess was higher than the number.')
+    else:
+        print('Your guess was lower than the number.')
+
+
+    trys -= 1
+    if trys == 0:
+        break
+    print(f'You have {trys} trys left')
 
     again = input('Enter q to exit or enter to take another guess.')
 
-if again == 'q':
+if again == 'q' or trys == 0:
     print(f"Sorry! The number was {number}.")
