5d217ddeef6a50ee1125d5f450d12c6d9144152c
Omar <bomarm567@gmail.com>
2024-02-08 10:42:11 -0600

    Allows user to take as many guesses wanted

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,14 @@
 number = 10
 
-print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+again = True
+while again != 'q':
+    print("I'm thinking of a number...")
+    guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    again = input('Enter q to exit or enter to take another guess.')
+
+if again == 'q':
+    print(f"Sorry! The number was {number}.")
