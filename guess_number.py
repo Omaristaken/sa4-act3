ce81a0006d607bc62dd889f782d5ce4da59b0adb
Omar <bomarm567@gmail.com>
2024-02-08 11:01:02 -0600

    Limits the number of hints user gets

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,8 @@
 number = 10
 
 again = True
+count = 5
+
 while again != 'q':
     print("I'm thinking of a number...")
     guess = int(input("What number am I thinking of? "))
@@ -8,6 +10,15 @@ while again != 'q':
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+ 
+    if count > 0:
+        if guess > number:
+            print('Your guess was higher than the number.')
+        else:
+            print('Your guess was lower than the number.')
+        count -= 1
+        print(f'You have {count} hints left')
+
     again = input('Enter q to exit or enter to take another guess.')
 
 if again == 'q':
