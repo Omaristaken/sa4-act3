25fb7d28ba712532f47e5b9d15a28af329f57e11
Omar <bomarm567@gmail.com>
2024-02-08 10:48:57 -0600

    Tells user if guess was higher or lower than number

--- guess_number.py
+++ guess_number.py
@@ -8,6 +8,10 @@ while again != 'q':
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+    if guess > number:
+        print('Your guess was higher than the number.')
+    else:
+        print('Your guess was lower than the number.')
     again = input('Enter q to exit or enter to take another guess.')
 
 if again == 'q':
